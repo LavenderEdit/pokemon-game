acdf808e64880eed96ff5a9fb525ad6a5648d184
LavenderEdit <gercermagden@gmail.com>
2026-05-29 13:51:19 -0500

    feat: changed the main file for handling on the game core engine

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from src.core.game import Game
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
