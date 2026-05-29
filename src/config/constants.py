2ee424cc24e288a481aa338e69dc6f6d0dce12c0
LavenderEdit <gercermagden@gmail.com>
2026-05-29 15:49:22 -0500

    feat: added the constants for link room

--- src/config/constants.py
+++ src/config/constants.py
@@ -1,35 +1,39 @@
 import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+ASSETS_DIR = os.path.join(BASE_DIR, "assets")
+DATA_DIR = os.path.join(BASE_DIR, "data")
+
 # Screen Dimensions
-WINDOW_WIDTH = 480
-WINDOW_HEIGHT = 320
-SCALE_FACTOR = 2
+WINDOW_WIDTH = 240
+WINDOW_HEIGHT = 160
+SCALE_FACTOR = 4 
 
 # Colors 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-# Asset Paths 
-ASSETS_DIR = "assets"
-DATA_DIR = "data"
+# Game States 
+STATE_OVERWORLD = "OVERWORLD"
+STATE_BATTLE = "BATTLE"
 
-# Backgrounds
+# Directions 
+DIR_DOWN = "DOWN"
+DIR_UP = "UP"
+DIR_LEFT = "LEFT"
+DIR_RIGHT = "RIGHT"
+
+# Asset Paths 
 BATTLE_BG_PATH = os.path.join(ASSETS_DIR, "backgrounds", "battle_bgs.png")
 LINK_ROOM_PATH = os.path.join(ASSETS_DIR, "backgrounds", "link_room_battle.png")
 
-# Sprites
+# Background crop coordinates (X, Y, Width, Height)
+LINK_ROOM_RECT = (334, 2, 353, 283)
+
 POKEMON_SPRITES_PATH = os.path.join(ASSETS_DIR, "sprites", "pokemon", "pokemon_gen1.png")
 PLAYER_SPRITE_PATH = os.path.join(ASSETS_DIR, "sprites", "player", "player_spritesheet.png")
-TRAINERS_BATTLE_PATH = os.path.join(ASSETS_DIR, "sprites", "trainers", "trainers_battle.png")
-
-# UI
 BATTLE_UI_PATH = os.path.join(ASSETS_DIR, "ui", "battle_ui.png")
 FONT_PATH = os.path.join(ASSETS_DIR, "fonts", "pokemon_fire_red.ttf")
 
-# Databases
-POKEMON_DB_PATH = os.path.join(DATA_DIR, "pokemon.json")
-MOVES_DB_PATH = os.path.join(DATA_DIR, "moves.json")
-EVOLUTIONS_DB_PATH = os.path.join(DATA_DIR, "evolutions.json")
-
-# FPS Limit
+# Performance 
 FPS = 60
