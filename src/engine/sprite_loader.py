80c21a402ff6c1072b7734aae7cb7d218fd722de
LavenderEdit <gercermagden@gmail.com>
2026-05-29 15:49:50 -0500

    feat: added the sprite loader enhancement

--- src/engine/sprite_loader.py
+++ src/engine/sprite_loader.py
@@ -10,13 +10,13 @@ class SpriteSheet:
 
     def image_at(self, rectangle, colorkey=None):
         rect = pygame.Rect(rectangle)
-        image = pygame.Surface(rect.size, pygame.SRCALPHA).convert_alpha()
         
+        image = pygame.Surface(rect.size, pygame.SRCALPHA)
         image.blit(self.sheet, (0, 0), rect)
         
         if colorkey is not None:
             if colorkey == -1:
-                colorkey = image.get_at((0, 0))
+                colorkey = self.sheet.get_at((0, 0))
             image.set_colorkey(colorkey, pygame.RLEACCEL)
             
         return image
@@ -24,10 +24,10 @@ class SpriteSheet:
     def images_at(self, rects, colorkey=None):
         return [self.image_at(rect, colorkey) for rect in rects]
 
-    def load_grid_images(self, num_x, num_y, start_x=0, start_y=0, sprite_w=64, sprite_h=64):
+    def load_grid_images(self, num_x, num_y, start_x=0, start_y=0, sprite_w=64, sprite_h=64, colorkey=None):
         images = []
         for y in range(num_y):
             for x in range(num_x):
                 rect = (start_x + (x * sprite_w), start_y + (y * sprite_h), sprite_w, sprite_h)
-                images.append(self.image_at(rect))
+                images.append(self.image_at(rect, colorkey))
         return images
